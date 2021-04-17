38fa758ba2b28962678516fcb7d6bc3bb40ccade
Piper McCorkle <contact@piperswe.me>
2021-04-17 03:16:56 -0500

    Yet more robust (ditching some architectures for now though)

--- default.nix
+++ default.nix
@@ -1,43 +1,26 @@
 { name ? "ghcr.io/h2eproject/nix-docker-multiarch"
-, cmd ? ({ hello }: "${hello}/bin/hello"), tagBase ? "latest", nixpkgs ? import
-  (builtins.fetchTarball
-    "https://github.com/h2eproject/nixpkgs/archive/refs/heads/piper/add-docker-arm-variant-support.tar.gz")
-, config ? { } }:
+, cmd ? ({ hello }: "${hello}/bin/hello"), tagBase ? "latest"
+, nixpkgs ? import <nixpkgs>, config ? { } }:
 
 let
   pkgs = nixpkgs { };
   lib = pkgs.lib;
-  muslSupported = arch:
-    arch == "i686" || arch == "x86_64" || arch == "aarch64" || arch
-    == "powerpc64le";
-  legacyArm = arch: arch == "armv5tel" || arch == "armv6l" || arch == "armv7l";
   buildImage = arch:
-    { dockerTools, callPackage, pkgsStatic }:
+    { dockerTools, callPackage }:
     dockerTools.buildImage {
       inherit name;
       tag = "${tagBase}-${arch}";
       config = {
-        Cmd = [
-          ((if muslSupported arch then pkgsStatic.callPackage else callPackage)
-            cmd { })
-        ];
+        Cmd = [ (callPackage cmd { }) ];
       } // config;
     };
-  architectures = [ "i686" "x86_64" "armv5tel" "armv6l" "armv7l" "aarch64" ];
+  architectures = [ "x86_64" "aarch64" ];
   crossSystems = map (arch: {
     inherit arch;
-    pkgs = nixpkgs {
-      crossSystem = {
-        config = "${arch}-unknown-linux-${
-            if muslSupported arch then
-              "musl"
-            else if legacyArm arch then
-              "gnueabi"
-            else
-              "gnu"
-          }";
-      };
-    };
+    pkgs = if pkgs.system == "${arch}-linux" then
+      pkgs
+    else
+      nixpkgs { crossSystem = { config = "${arch}-linux"; }; };
   }) architectures;
   images = map ({ arch, pkgs }: rec {
     inherit arch;
--- usage-example.nix
+++ usage-example.nix
@@ -1,10 +1,12 @@
-{ name ? "ghcr.io/h2eproject/nix-docker-multiarch-example", tagBase ? "latest" }:
-let docker = import ./default.nix;
+{ name ? "ghcr.io/h2eproject/nix-docker-multiarch-example", tagBase ? "latest"
+}:
+let
+  docker = import ./default.nix;
   # Use this to include from other projects
   dockerFromGit = import (builtins.fetchGit {
-      url = "https://github.com/h2eproject/nix-docker-multiarch";
-      ref = "main";
-    });
+    url = "https://github.com/h2eproject/nix-docker-multiarch";
+    ref = "main";
+  });
 in docker {
   # The name of the image
   inherit name;
@@ -15,9 +17,5 @@ in docker {
   cmd = { python3 }: "${python3}/bin/python3";
   # Additional configuration for the image
   # See https://github.com/moby/moby/blob/master/image/spec/v1.2.md#container-runconfig-field-descriptions
-  config = {
-    Env = [
-      "NIX_DOCKER_MULTIARCH=\"true\""
-    ];
-  };
+  config = { Env = [ ''NIX_DOCKER_MULTIARCH="true"'' ]; };
 }
